a9283fc3692a42aa419e97ad559b6c57411f6ec1
Mathieu Derrit <mathieu.derrit@gmail.com>
2022-11-28 11:10:42 +0100

    State Machine Managers

---
+++ Assets/Scripts/Managers/GameStateMachine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateMachine : MonoBehaviour
+{
+    #region fields
+    private Dictionary<EgameState, BaseGameState> _statesGDict = new Dictionary<EgameState, BaseGameState>();
+    #endregion
+
+    #region Properties
+    public BaseGameState CurrentGState => _statesGDict[_curentGState];
+    private EgameState _curentGState;
+    private EgameState _lastGState;
+    private EgameState EgameState = EgameState.NONE;
+    public EgameState CurrentStateType => _curentGState;
+    public EgameState LastGState { get => _lastGState; set => _lastGState = value; }
+    #endregion
+
+    #region Methods
+
+    #region Start And Init
+    private void Start()
+    {
+        _statesGDict = new Dictionary<EgameState, BaseGameState>();
+        SubGStateInit();
+        CurrentGState.StartState();
+    }
+
+    private void SubGStateInit()
+    {
+        StartGameState startState = new StartGameState();
+        startState.Init(this, EgameState.START);
+        _statesGDict.Add(EgameState.START, startState);
+
+        MenuGameState menuState = new MenuGameState();
+        menuState.Init(this, EgameState.MENU);
+        _statesGDict.Add(EgameState.MENU, menuState);
+
+    }
+    #endregion
+
+    #region RunTime
+    private void Update()
+    {
+        CurrentGState.UpdateState();
+    }
+
+    private void FixedUpdate()
+    {
+        CurrentGState.FixedUpdateState();
+    }
+
+    public void changeState(EgameState nextState,bool switchSong)
+    {
+        CurrentGState.LeaveState();
+        _curentGState = nextState;
+        CurrentGState.StartState();
+    }
+
+    #endregion
+
+    #endregion
+}
+
+public enum EgameState
+{
+    START,
+    MENU,
+    NONE
+}
+
---
+++ Assets/Scripts/Managers/MenuGameState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuGameState : BaseGameState
+{
+    #region fileds
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods   
+    public override void StartState()
+    {
+        _machine.LastGState = EgameState.MENU;
+    }
+
+    public override void UpdateState()
+    {
+
+    }
+
+    public override void FixedUpdateState()
+    {
+
+    }
+
+    public override void LeaveState()
+    {
+        _machine.LastGState = EgameState.NONE;
+    }
+
+    #endregion
+}
